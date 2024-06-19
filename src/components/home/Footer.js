ae5db85a91fe3d996a9a390fad9d0c141bfff1cd
Bhupendra <bhupendra.scripts@gmail.com>
2024-06-19 17:51:53 +0530

    added icons

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
-import BurgerIcon from "./BurgerIcon";
+import Facebook from "../../assest/facebook.svg";
+import Twitter from "../../assest/twitter.svg";
+import Insta from "../../assest/insta.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="w-full py-4 px-5 md:px-20 bg-dark-navy flex flex-col sm:flex-row gap-5 items-center justify-between">
+    <div className="w-full py-8 px-5 md:px-20 bg-dark-navy flex flex-col sm:flex-row gap-5 items-center justify-between">
       <div className="flex flex-col s  m:flex-row items-center">
         <div className="py-4 text-3xl font-extrabold text-white">scoot</div>
         <ul className="sm:ml-10 flex flex-col sm:flex-row items-center text-dim-grey cursor-pointer">
@@ -12,10 +15,10 @@ const Footer = () => {
           <li className="p-3  hover:text-yellow">Careers</li>
         </ul>
       </div>
-      <div className="flex">
-        <BurgerIcon/>
-        <BurgerIcon/>
-        <BurgerIcon/>
+      <div className="flex gap-6 my-5 hover:*:scale-110 *:duration-75 *:cursor-pointer">
+      <img alt="social media" src={Facebook} width="24px"/>
+      <img alt="social media" src={Twitter} width="24px"/>
+      <img alt="social media" src={Insta} width="24px"/>
       </div>
     </div>
   );
--- src/components/home/GuideSection.js
+++ src/components/home/GuideSection.js
@@ -1,15 +1,18 @@
 import React from "react";
-
+import Icon1 from '../../assest/home/mobile.svg';
+import Icon2 from '../../assest/home/riding-scooter.svg';
+import Icon3 from '../../assest/home/scooter.svg';
 const GuideSection = () => {
   return (
     <div className="py-10 lg:flex overflow-hidden">
+
       <div className="my-4 p-5 text-center sm:text-start sm:flex sm:justify-center sm:items-start sm:max-w-[750px] sm:mx-auto lg:inline-block">
-        <div className="mx-auto mb-6 w-24 h-24 bg-yellow rounded-full">
-          <img alt="icon" />
+        <div className="mx-auto mb-6 w-24 h-24 flex items-center justify-center bg-yellow rounded-full">
+          <img alt="icon" src={Icon1} width="45px"/>
         </div>
         <div className="sm:basis-3/4">
           <h4 className="text-2xl font-bold text-dark-navy">Locate with app</h4>
-          <p className="mt-6 text-dim-grey">
+          <p className="mt-6 text-dim-grey text-justify">
             Use the app to find the nearest scooter to you. We are continuously
             placing scooters in the areas with most demand, so one should never
             be too far away.
@@ -17,35 +20,29 @@ const GuideSection = () => {
         </div>
       </div>
       <div className="my-4 p-5 text-center sm:text-start sm:flex sm:justify-center sm:items-start sm:max-w-[750px] sm:mx-auto lg:inline-block">
-        <div className="mx-auto mb-6 w-24 h-24 bg-yellow rounded-full">
-          <img alt="icon" />
+        <div className="mx-auto mb-6 w-24 h-24 flex items-center justify-center bg-yellow rounded-full">
+          <img alt="icon" src={Icon2} width="45px"/>
         </div>
         <div className="sm:basis-3/4">
-          <h4 className="text-2xl font-bold text-dark-navy">Locate with app</h4>
-          <p className="mt-6 text-dim-grey">
-            Use the app to find the nearest scooter to you. We are continuously
-            placing scooters in the areas with most demand, so one should never
-            be too far away.
+          <h4 className="text-2xl font-bold text-dark-navy">Pick your scooter</h4>
+          <p className="mt-6 text-dim-grey text-justify">
+          We show the most important info for the scooters closest to you. So you know how much charge they have left and can see roughly how much it will cost.
           </p>
         </div>
       </div>
       <div className="my-4 p-5 text-center sm:text-start sm:flex sm:justify-center sm:items-start sm:max-w-[750px] sm:mx-auto lg:inline-block">
-        <div className="relative mx-auto mb-6 w-24 h-24 bg-yellow rounded-full ">
-          <img alt="icon" />
-          {/* The grey line in back of the icons */}
+        <div className="relative mx-auto mb-6 w-24 h-24 flex items-center justify-center bg-yellow rounded-full ">
+          <img alt="icon" src={Icon3} width="45px"/>
+          {/*Below is for the grey line in back of the icons */}
           <div className="hidden sm:inline absolute left-1/2 top-1/2 origin-left -rotate-90 lg:rotate-180 h-4 w-[14000px] bg-light-grey -z-50"></div>
         </div>
         <div className="sm:basis-3/4">
-          <h4 className="text-2xl font-bold text-dark-navy">Locate with app</h4>
-          <p className="mt-6 text-dim-grey">
-            Use the app to find the nearest scooter to you. We are continuously
-            placing scooters in the areas with most demand, so one should never
-            be too far away.
+          <h4 className="text-2xl font-bold text-dark-navy">Enjoy the ride</h4>
+          <p className="mt-6 text-dim-grey text-justify">
+          Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet, and you’re off! Always lock bikes away from walkways and accessibility ramps.
           </p>
         </div>
       </div>
-
-
 
     </div>
   );
