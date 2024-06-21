888b849d07eb4303743344fcad0bab68b60fb5b4
deepakkumar1211 <sahudeepak1211.com>
2024-06-22 00:33:12 +0530

    Added props in InfoSection

--- src/components/home/InfoSection.js
+++ src/components/home/InfoSection.js
@@ -4,9 +4,9 @@ import InfoChild from "./InfoChild";
 const InfoSection = () => {
   return (
     <div className="mb-4">
-    <InfoChild/>
-    <InfoChild/>
-    <InfoChild/>
+    <InfoChild btnText="Learn More" mainInfo="Easy to use riding telemetry" />
+    <InfoChild btnText="Visit me" mainInfo="Visit me to use riding essentials" />
+    <InfoChild btnText="Click me" mainInfo="Click me to find best scooty in rent" />
 
     </div>
   );
